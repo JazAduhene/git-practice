0d25c735b77493314cf80f11a7ad00fe174f9714
JazAduhene <jasmine@unknown.home>
2020-10-14 20:50:41 +0100

    Second Commit Variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+// 1. You cant define a variable more than once
+let petName = 'Vera'
+petName = "Spot"
+petName = "Edlee"
+
+console.log(petName)
+
+// 2. There are rules related to variable names
+// Start with letters A-Z, _ OR $ , cannot start with numbers or any other symbols
+
+// 3. Variable names cannot be reserved keywords
+// Cannot use let let = 12
